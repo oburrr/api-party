6e87a27019c6239afdab13a8601f192dc920988d
Olivia Burris <oliviaburris11@gmail.com>
2017-07-11 10:51:04 -0400

    Create Github component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch, NavLink } from 'react-router-dom'
 import './App.css';
 
+import Github from './Github'
+
 class App extends Component {
   render() {
     return (
@@ -11,19 +13,18 @@ class App extends Component {
             <h3>Ain't no party like an</h3>
             <h1>API Party</h1>
           </div>
-          <ul className='nav-links'>
+          <ul className="nav-links">
             <li>
-             <NavLink to='/github'>Github API</NavLink>
+              <NavLink to='/github'>Github API</NavLink>
             </li>
           </ul>
         </div>
         <Switch>
-          <Route path='/github' render={() => <h1>Github!</h1>} />
-          <Route  render={ () => <p>To get started, click one of the links above</p>} />
+          <Route path='/github' component={Github} />
+          <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
     );
   }
 }
-
 export default App;
